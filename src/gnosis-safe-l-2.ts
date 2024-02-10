02d863178cec3f056e5d32fc2a46f8141fd7047c
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2024-02-09 20:43:27 -0500

    revised event handleSignMsg

--- src/gnosis-safe-l-2.ts
+++ src/gnosis-safe-l-2.ts
@@ -292,16 +292,20 @@ export function handleSafeSetup(event: SafeSetupEvent): void {
 }
 
 export function handleSignMsg(event: SignMsgEvent): void {
-  // Directly use the transaction hash as the entity ID without converting it to a hex string
+  // Create a new SignMsg entity with a string ID
   let entity = new SignMsg(event.transaction.hash.toHex())
 
+  // Assuming msgHash is of type Bytes and event.params.msgHash is also Bytes
   entity.msgHash = event.params.msgHash
-  // For the signer, assuming you're trying to convert an Ethereum address to Bytes
-  // Use the .toHexString() method on the Address type, and then convert it to Bytes
+
+  // If the signer is expected to be a Bytes type and event.transaction.from is an Address
+  // Convert Address to Bytes using toHex() for conversion, then to Bytes
   entity.signer = Bytes.fromHexString(event.transaction.from.toHex())
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
+  // Directly assign transactionHash if it's already Bytes; no need for conversion
   entity.transactionHash = event.transaction.hash
+
   entity.save()
 }
